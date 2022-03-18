33c920e44f603335363018cbb4e954cf80ced5d7
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-17 21:14:26 -0700

    added array variables to the function

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,10 +1,24 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let special = ["@", "$", "#", "%", "(", ")", "-", "^", "&", "*", "_", "=", "+"];
+  let endPass = []
+  let lengthPrompt = prompt("Choose a password length between 8 and 128 characters")
+  let numbPrompt = confirm("Would you like to include numbers in your password?")
+  let upperPrompt = confirm("Would you like to include uppercase letters in your password?")
+  let lowerPrompt = confirm("Would you like to include lowercase letters in your password?")
+  let specialPrompt = confirm("Would you like to include special characters in your password?")
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
