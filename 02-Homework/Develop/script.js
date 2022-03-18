962903e26955b8f85fe5355f0af2ba4d8022d2a2
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-17 22:05:37 -0700

    added if statements, started attempting a for loop

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,17 +2,43 @@
 let generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   let special = ["@", "$", "#", "%", "(", ")", "-", "^", "&", "*", "_", "=", "+"];
-  let endPass = []
-  let lengthPrompt = prompt("Choose a password length between 8 and 128 characters")
-  let numbPrompt = confirm("Would you like to include numbers in your password?")
-  let upperPrompt = confirm("Would you like to include uppercase letters in your password?")
-  let lowerPrompt = confirm("Would you like to include lowercase letters in your password?")
-  let specialPrompt = confirm("Would you like to include special characters in your password?")
+  let endPass = [];
+  let emptyArr = [];
+  upperChar[1];
+  let lengthPrompt = prompt("Choose a password length between 8 and 128 characters");
+  let upperPrompt = confirm("Would you like to include uppercase letters in your password?");
+  let lowerPrompt = confirm("Would you like to include lowercase letters in your password?");
+  let numbPrompt = confirm("Would you like to include numbers in your password?");
+  let specialPrompt = confirm("Would you like to include special characters in your password?");
 
+  // 
+
+  if (upperPrompt) {
+    endPass = endPass.concat(upperChar);
+  }
+
+  if (lowerPrompt) {
+    endPass = endPass.concat(lowerChar);
+  }
+
+  if (numbPrompt) {
+    endPass = endPass.concat(numbers);
+  }
+
+  if (specialPrompt) {
+    endPass = endPass.concat(special);
+  }
+  console.log(endPass);
+
+  //if (lengthPrompt > 7 && lengthPrompt < 128) {
+ 
+  //} else {
+  // alert("Your password must be a length between 8 and 128 characters")
+  //}
 }
 
 // Write password to the #password input
